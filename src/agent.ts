5e3b7c6dba93438fd18b6b6034933d3c20a6936b
Yehia Tarek <yehia@startfi.io>
2022-05-02 22:07:10 +0200

    use methodID instead of checking for contract deployment transaction

--- src/agent.ts
+++ src/agent.ts
@@ -18,38 +18,20 @@ const handleTransaction: HandleTransaction = async (
 
   if (!isDeployer) return findings;
 
-  // Check if its a deployment transaction
-  // to should be equal null and contract address should exist
-  if (
-    txEvent.transaction.to === null &&
-    txEvent.contractAddress &&
-    txEvent.contractAddress?.length > 0
-  ) {
-
-    // Make sure the agent ERC721 token has been minted
-    const erc721TransferEvents = txEvent.filterLog(ERC721_TRANSFER_EVENT);
-
-    erc721TransferEvents.forEach((transferEvent) => {
-      // extract transfer event arguments
-      const { to, from } = transferEvent.args;
-      if (
-        from === "0x0000000000000000000000000000000000000000" &&
-        to === DEPLOYER
-      ) {
-        findings.push(
-          Finding.fromObject({
-            name: "The Nethermind Forta deployer",
-            description: `Forta deployer account with address ${DEPLOYER} deploy new bot with address: ${txEvent.contractAddress}`,
-            alertId: "DEPLOYER-0",
-            type: FindingType.Info,
-            severity: FindingSeverity.Info,
-            metadata: {
-              contractAddress: txEvent.contractAddress!,
-            },
-          })
-        );
-      }
-    });
+  const methodID = "0x7935d5b4";
+  if (methodID === txEvent.transaction.data.slice(0, 10)) {
+    findings.push(
+      Finding.fromObject({
+        name: "The Nethermind Forta deployer",
+        description: `Forta deployer account with address ${DEPLOYER} deploy new bot with address: ${txEvent.contractAddress}`,
+        alertId: "DEPLOYER-0",
+        type: FindingType.Info,
+        severity: FindingSeverity.Info,
+        metadata: {
+          contractAddress: txEvent.contractAddress!,
+        },
+      })
+    );
   }
 
   return findings;
